318bd99ebbb213a037f4e1a037c03c4a701e6eaf
morgan4563 <morganpark4563@gmail.com>
2025-06-08 23:44:45 +0900

    feat: CalendarSelectManager 메서드 추가
    isInSelectedRange : 해당 날짜가 선택된 날짜 구간에 포함되는지 확인하는 메서드
    
    isStartOrEndDate: 해당 날짜가 StartDate혹은 EndDate인지 확인하는 메서드

--- EST_Trip/Feature/Calendar/Manager/CalendarSelectionManager.swift
+++ EST_Trip/Feature/Calendar/Manager/CalendarSelectionManager.swift
@@ -10,6 +10,7 @@ import Foundation
 class CalendarSelectionManager {
     private var travelDate = TravelDate()
 
+    // 달력의 날짜 선택
     func select(date: Date) {
         // 아무 날짜도 선택이 안된 상태
         if travelDate.startDate == nil {
@@ -22,7 +23,6 @@ class CalendarSelectionManager {
             if date == travelDate.startDate {
                 return
             }
-
             // 새로 선택한 날짜가 startDate보다 이전이면 endDate와 스왑해서 설정하기
             guard let startDate = travelDate.startDate else { return }
             if date < startDate {
@@ -37,5 +37,17 @@ class CalendarSelectionManager {
             travelDate.startDate = date
             travelDate.endDate = nil
         }
+    }
+
+    // 해당 날짜가 선택된 날짜 구간에 포함되는지 확인 (선 UI보여줘야 함)
+    func isInSelectedRange(_ date: Date) -> Bool {
+		guard let startDate = travelDate.startDate,
+              let endDate = travelDate.endDate else { return false }
+        return date > startDate && date < endDate
+    }
+
+    // 해당 날짜가 Start혹은 endDate인지 확인 (해당 날짜가 StartDate이나 EndDate이면 다른 UI를 적용해야함)
+    func isStartOrEndDate(_ date: Date) -> Bool {
+        return date == travelDate.startDate || date == travelDate.endDate
     }
 }
