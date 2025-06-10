4824adc8deb49771436ae69cbf32ce2d38cd4c72
morgan4563 <morganpark4563@gmail.com>
2025-06-10 22:33:07 +0900

    feat: 스택 요소 제약 추가

---
+++ EST_Trip/Feature/FlightAdd/View/FlightAddViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FlightAddViewController.swift
+//  EST_Trip
+//
+//  Created by hyunMac on 6/10/25.
+//
+
+import UIKit
+
+class FlightAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
