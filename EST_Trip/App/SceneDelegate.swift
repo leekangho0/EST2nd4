b0722ab8d5610416eb3de37f52f0ec292e469f78
morgan4563 <morganpark4563@gmail.com>
2025-06-13 14:27:52 +0900

    refactor: CalendarManagerr를 CalendarViewModel에 통합

--- EST_Trip/App/SceneDelegate.swift
+++ EST_Trip/App/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-        let root = FeatureFactory.makeSearch()
+        let root = FeatureFactory.makeCalendar()
         let nav = UINavigationController(rootViewController: root)
         window.rootViewController = nav
         window.makeKeyAndVisible()
