9e4175125a17e0654d992126cc3e9072ad422d47
kangho Lee <ibcylon@naver.com>
2025-06-18 21:18:29 +0900

    Fix: bind header text

--- EST_Trip/Data/DTOs/GooglePlaceDTO.swift
+++ EST_Trip/Data/DTOs/GooglePlaceDTO.swift
@@ -5,4 +5,24 @@
 //  Created by kangho lee on 6/18/25.
 //
 
-import Foundation
+import UIKit
+import GooglePlacesSwift
+
+struct GooglePlaceDTO {
+    let place: Place
+    var image: UIImage?
+}
+
+extension GooglePlaceDTO {
+    func apply(_ entity: PlaceEntity) {
+        entity.latitude = place.location.latitude
+        entity.longitude = place.location.longitude
+        entity.name = place.displayName
+        entity.reviewCount = Int16(place.numberOfUserRatings)
+        entity.rating = Double(place.rating ?? 0.0)
+        entity.photo = image?.jpegData(compressionQuality: 1)
+        entity.categoryType = CategoryType.from(placeTypes: place.types.map(\.rawValue))
+        entity.address = place.formattedAddress
+        entity.id = UUID(uuidString: place.placeID ?? UUID().uuidString) ?? UUID()
+    }
+}
