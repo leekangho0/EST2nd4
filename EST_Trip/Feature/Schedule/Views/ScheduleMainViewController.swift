7b78b1ed1d7b6a265a0eb8b066f860029b3f4315
8zipcore <howtofindjayu@gmail.com>
2025-06-18 22:28:48 +0900

    다크 모드 대응

--- EST_Trip/Feature/Schedule/Views/ScheduleMainViewController.swift
+++ EST_Trip/Feature/Schedule/Views/ScheduleMainViewController.swift
@@ -176,8 +176,8 @@ extension ScheduleMainViewController {
         toggleButtons.forEach { button in
             button.layer.cornerRadius = button.frame.height / 2
             button.clipsToBounds = true
-            button.layer.backgroundColor = UIColor(hex: "#D9D9D9", alpha: 0.6)?.cgColor
-            button.setTitleColor(UIColor(hex: "#7E7E7E", alpha: 1.0), for: .normal)
+//            button.layer.backgroundColor = UIColor(hex: "#D9D9D9", alpha: 0.6)?.cgColor
+//            button.setTitleColor(UIColor(hex: "#7E7E7E", alpha: 1.0), for: .normal)
         }
     }
     
